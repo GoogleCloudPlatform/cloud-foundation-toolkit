a9f1665b32c9079565ffc3e38150723a3fa67dad
Andriy Kopachevskyy <kopachevsky@gmail.com>
2019-05-25 12:55:16 +0300

    regexp search for cross deployment references

--- cli/deployment_test.go
+++ cli/deployment_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"path/filepath"
+	"reflect"
 	"testing"
 )
 
@@ -10,5 +11,25 @@ func TestNewConfig(t *testing.T) {
 	config := NewConfig(path)
 	if config == nil {
 		t.Errorf("Config is nil")
+	}
+}
+
+func TestFindAllOutRefs(t *testing.T) {
+	executeFindAllOutRefsAndAssert(
+		`network: $(out.project1.resource1.output1)
+                    other: $(out.resource2.output2)`,
+		[]string{"$(out.project1.resource1.output1)", "$(out.resource2.output2)"},
+		t)
+	// empty string
+	executeFindAllOutRefsAndAssert("", nil, t)
+	// invalid notation
+	executeFindAllOutRefsAndAssert("${out1.account.project.resource.output", nil, t)
+}
+
+func executeFindAllOutRefsAndAssert(yaml_string string, expected []string, t *testing.T) {
+	config := &Config{yaml_string: yaml_string}
+	actual := config.findAllOutRefs()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got: %v, expected: %v.", actual, expected)
 	}
 }
