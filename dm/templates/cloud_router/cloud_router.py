a4a1ef8e782252b712190033d01ac50a67e862d7
Bohdan Yurov <bohdan.yurov@globallogic.com>
2019-05-26 07:18:27 +0300

    Fixes #47 for: - templates/cloud_router
    https://github.com/GoogleCloudPlatform/cloud-foundation-toolkit/issues/51
    https://github.com/GoogleCloudPlatform/cloud-foundation-toolkit/issues/47

--- dm/templates/cloud_router/cloud_router.py
+++ dm/templates/cloud_router/cloud_router.py
@@ -14,6 +14,14 @@
 """ This template creates a Cloud Router. """
 
 
+def append_optional_property(res, properties, prop_name):
+    """ If the property is set, it is added to the resource. """
+
+    val = properties.get(prop_name)
+    if val:
+        res['properties'][prop_name] = val
+    return
+
 def generate_config(context):
     """ Entry point for the deployment resources. """
 
@@ -21,33 +29,41 @@ def generate_config(context):
     name = properties.get('name', context.env['name'])
     project_id = properties.get('project', context.env['project'])
 
-    resources = [
-        {
-            'name': context.env['name'],
-            'type': 'gcp-types/compute-v1:routers',
-            'properties':
-                {
-                    'name':
-                        name,
-                    'bgp': {
-                        'asn': context.properties['asn']
-                    },
-                    'network':
-                        generate_network_url(
-                            project_id,
-                            context.properties['network']
-                        ),
-                    'region':
-                        context.properties['region'],
-                    'project':
-                        project_id
-                }
-        }
+    bgp = properties.get('bgp', {'asn': properties.get('asn')})
+
+    router = {
+        'name': context.env['name'],
+        # https://cloud.google.com/compute/docs/reference/rest/v1/routers
+        'type': 'gcp-types/compute-v1:routers',
+        'properties':
+            {
+                'name':
+                    name,
+                'project':
+                    project_id,
+                'region':
+                    properties['region'],
+                'bgp': bgp,
+                'network':
+                    generate_network_url(
+                        project_id,
+                        properties['network']
+                    ),
+            }
+    }
+
+    optional_properties = [
+        'description',
+        'bgpPeers',
+        'interfaces',
+        'nats',
     ]
 
+    for prop in optional_properties:
+        append_optional_property(router, properties, prop)
+
     return {
-        'resources':
-            resources,
+        'resources': [router],
         'outputs':
             [
                 {
