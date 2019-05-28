646cdaa256c7880ed74cba189f46746b79e0bc7c
Andriy Kopachevskyy <kopachevsky@gmail.com>
2019-05-28 15:27:14 +0300

    [dm] [cli] added basic Create command

--- cli/deployment/api_client_test.go
+++ cli/deployment/api_client_test.go
@@ -1,19 +1,10 @@
 package deployment
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
-
-/*
-func testGCloud(t *testing.T) {
-	res, err := GCloud("deployment-manager", "deployments", "describe", "gl-akopachevsky-test")
-	if err != nil {
-		t.Error(err)
-	}
-
-	fmt.Println(res)
-}*/
 
 func TestGetOutputs(t *testing.T) {
 	execFunction = func(args ...string) (result string, err error) {
@@ -33,4 +24,24 @@ func TestGetOutputs(t *testing.T) {
 	if "my-network-prod" != outputs["my-network-prod.name"] {
 		t.Errorf("Expected \"my-network-prod\" got \"%s\"", outputs["my-network-prod.name"])
 	}
+}
+
+func TestCreate(t *testing.T) {
+	dep := Deployment{
+		configFile: "/1-prj/projects/google/CFT/fork/cloud-foundation-toolkit/dm/network.yaml",
+		config: Config{
+			Project: "gl-akopachevskyy-dm-seed",
+			Name:    "my-first-auto-deployment",
+		},
+	}
+	deployment, err := Create(&dep)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(deployment.outputs) == 0 {
+		t.Errorf("Should be more outputs")
+	}
+
+	fmt.Println(deployment)
 }
