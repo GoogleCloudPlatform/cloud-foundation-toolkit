4e81556fd35708f911dedc6f7b79a4e4b39c1408
Jeff McCune <jeff@openinfrastructure.co>
2019-07-11 12:00:22 -0700

    Separate required IAM roles by project and folder
    Establishes a sub-folder specific to IAM so that IAM permissions are not
    granted broadly across all module resources.  Changes API service
    managed to be additive instead of authoritative.
    
    Note, the organization level roles are intentially skipped in order to
    evaluate the impact and risk of granting IAM admin.

--- infra/terraform/test_fixtures/terraform-google-iam.tf
+++ infra/terraform/test_fixtures/terraform-google-iam.tf
@@ -1,34 +1,28 @@
 locals {
   // Roles required by CI service accounts to run the IAM
   // integration tests.
-  iam_required_roles = [
-    "roles/resourcemanager.organizationAdmin",
+  iam_required_project_roles = [
     "roles/storage.admin",
     "roles/pubsub.admin",
     "roles/cloudkms.admin",
     "roles/compute.admin",
     "roles/compute.networkAdmin",
-    "roles/resourcemanager.folderAdmin",
-    "roles/resourcemanager.folderIamAdmin",
     "roles/iam.serviceAccountAdmin",
     "roles/owner",
     "roles/resourcemanager.projectIamAdmin",
   ]
-}
+  iam_required_folder_roles = [
+    "roles/resourcemanager.folderAdmin",
+    "roles/resourcemanager.folderIamAdmin",
+  ]
+  # NOTE: Org level roles are presently unused to verify integration tests
+  # actually require oraganization admin, and if so evaluate if these tests
+  # should be skipped because they may be destructive.
+  # iam_required_organization_roles = [
+  #   "roles/resourcemanager.organizationAdmin",
+  # ]
 
-resource "google_project" "iam" {
-  provider = "google.phoogle"
-  name = "ci-iam"
-  project_id = "ci-iam"
-  folder_id = "${google_folder.phoogle_cloud_foundation_cicd.name}"
-  billing_account = "${module.variables.phoogle_billing_account}"
-}
-
-resource "google_project_services" "iam" {
-  provider = "google.phoogle"
-
-  project = "${google_project.iam.id}"
-  services = [
+  iam_required_api_services = [
     "admin.googleapis.com",
     "appengine.googleapis.com",
     "cloudbilling.googleapis.com",
@@ -44,6 +38,30 @@ resource "google_project_services" "iam" {
   ]
 }
 
+# Creating a dedicated folder to exercise folder level tests
+resource "google_folder" "iam" {
+  provider = "google.phoogle"
+
+  display_name = "ci-iam"
+  parent       = "${google_folder.phoogle_cloud_foundation_cicd.name}"
+}
+
+resource "google_project" "iam" {
+  provider = "google.phoogle"
+  name = "ci-iam"
+  project_id = "ci-iam"
+  folder_id = "${google_folder.iam.name}"
+  billing_account = "${module.variables.phoogle_billing_account}"
+}
+
+resource "google_project_service" "iam" {
+  provider = "google.phoogle"
+
+  count   = "${length(local.iam_required_api_services)}"
+  project = "${google_project.iam.id}"
+  service = "${element(local.iam_required_api_services, count.index)}"
+}
+
 resource "google_service_account" "iam" {
   provider = "google.phoogle"
   project      = "${google_project.iam.id}"
@@ -53,10 +71,18 @@ resource "google_service_account" "iam" {
 
 resource "google_folder_iam_member" "iam" {
   provider = "google.phoogle"
-  count = "${length(local.iam_required_roles)}"
-  folder = "${google_folder.phoogle_cloud_foundation_cicd.name}"
-  role   = "${element(local.iam_required_roles, count.index)}"
-  member = "serviceAccount:${google_service_account.iam.email}"
+  count    = "${length(local.iam_required_folder_roles)}"
+  folder   = "${google_folder.iam.name}"
+  role     = "${element(local.iam_required_folder_roles, count.index)}"
+  member   = "serviceAccount:${google_service_account.iam.email}"
+}
+
+resource "google_project_iam_member" "iam" {
+  provider = "google.phoogle"
+  count    = "${length(local.iam_required_project_roles)}"
+  project  = "${google_project.iam.project_id}"
+  role     = "${element(local.iam_required_project_roles, count.index)}"
+  member   = "serviceAccount:${google_service_account.iam.email}"
 }
 
 resource "google_service_account_key" "iam" {
