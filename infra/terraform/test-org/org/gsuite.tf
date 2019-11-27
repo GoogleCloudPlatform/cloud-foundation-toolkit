a627cfbe780d340f1d4f3efbd203dd5da22cd2c5
Pavel Zhukov <cray0000@gmail.com>
2019-11-27 21:19:45 +0200

    [test-org] Create long-living SA which has G-Suite access. Used for project-factory tests of gsuite functionality and can be used in other gsuite-related tests in other modules.

---
+++ infra/terraform/test-org/org/gsuite.tf
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  ci_gsuite_sa_project_roles = [
+    "roles/owner",
+    "roles/compute.admin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/storage.admin",
+    "roles/iam.serviceAccountUser",
+    "roles/billing.projectManager",
+  ]
+
+  ci_gsuite_sa_folder_roles = [
+    "roles/owner",
+    "roles/resourcemanager.projectCreator",
+    "roles/resourcemanager.folderAdmin",
+    "roles/resourcemanager.folderIamAdmin",
+    "roles/billing.projectManager",
+  ]
+}
+
+resource "google_folder" "ci_gsuite_sa_folder" {
+  display_name = "ci-gsuite-sa-folder"
+  parent      = "folders/${replace(local.folders["ci-projects"], "folders/", "")}"
+}
+
+module "ci_gsuite_sa_project" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 4.0"
+
+  name              = "ci-gsuite-sa-project"
+  random_project_id = true
+  org_id            = local.org_id
+  folder_id         = google_folder.ci_gsuite_sa_folder.id
+  billing_account   = local.billing_account
+
+  labels = {
+    cft-ci = "permanent"
+  }
+
+  activate_apis = [
+    "admin.googleapis.com",
+    "appengine.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "oslogin.googleapis.com",
+    "serviceusage.googleapis.com",
+  ]
+}
+
+resource "google_service_account" "ci_gsuite_sa" {
+  project      = module.ci_gsuite_sa_project.project_id
+  account_id   = "ci-gsuite-sa"
+  display_name = "ci-gsuite-sa"
+}
+
+resource "google_project_iam_member" "ci_gsuite_sa_project" {
+  count = length(local.ci_gsuite_sa_project_roles)
+
+  project = module.ci_gsuite_sa_project.project_id
+  role    = local.ci_gsuite_sa_project_roles[count.index]
+  member  = "serviceAccount:${google_service_account.ci_gsuite_sa.email}"
+}
+
+resource "google_folder_iam_member" "ci_gsuite_sa_folder" {
+  count = length(local.ci_gsuite_sa_folder_roles)
+
+  folder = google_folder.ci_gsuite_sa_folder.name
+  role   = local.ci_gsuite_sa_folder_roles[count.index]
+  member = "serviceAccount:${google_service_account.ci_gsuite_sa.email}"
+}
+
+resource "google_billing_account_iam_member" "ci_gsuite_sa_billing" {
+  billing_account_id = local.billing_account
+  role               = "roles/billing.user"
+  member             = "serviceAccount:${google_service_account.ci_gsuite_sa.email}"
+}
+
+resource "google_service_account_key" "ci_gsuite_sa" {
+  service_account_id = google_service_account.ci_gsuite_sa.id
+}
