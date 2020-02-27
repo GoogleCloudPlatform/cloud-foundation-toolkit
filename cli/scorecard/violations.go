a3f0b3b4322f7ec5a5dfc69872c37727a69e4d8a
Keshi Tan <keshi@google.com>
2020-02-28 00:03:49 +0800

    chore: Scorecard - add some tests  (#588)
    * remove unused function
    
    * add some tests for scorecard
    
    * cleanup file
    
    * refactor scorecard tests
    
    * style changes
    
    Co-authored-by: Morgante Pell <morgante.pell@morgante.net>

--- cli/scorecard/violations.go
+++ cli/scorecard/violations.go
@@ -144,22 +144,3 @@ func getAssetFromJSON(input []byte) (*validator.Asset, error) {
 	return pbAsset, nil
 }
 
-// listFiles returns a list of files under a dir. Errors will be grpc errors.
-func listFiles(dir string) ([]string, error) {
-	files := []string{}
-	visit := func(path string, f os.FileInfo, err error) error {
-		if err != nil {
-			return errors.Wrapf(err, "error visiting path %s", path)
-		}
-		if !f.IsDir() {
-			files = append(files, path)
-		}
-		return nil
-	}
-
-	err := filepath.Walk(dir, visit)
-	if err != nil {
-		return nil, err
-	}
-	return files, nil
-}
