08d3e5eac685681fab8d8555008f151b79b7b3f8
maqiuyujoyce <33072938+maqiuyujoyce@users.noreply.github.com>
2020-04-22 17:29:12 -0700

    [KCC] Add the Go CLI to test Config Connector solutions (#651)
    * Add the Go CLI to test Config Connector solutions
    
    Currently, it only runs the test for a single solution at a time.
    
    * Fix nits in README.md
    
    * Add go.mod and vendor folder to the ccs-test CLI
    
    * Update README based on the comments
    
    * Reword a sentence in README
    
    * Add License info into the files

---
+++ config-connector/tests/ccs-test/cmd/root.go
@@ -0,0 +1,307 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/ghodss/yaml"
+	"github.com/spf13/cobra"
+)
+
+const (
+	kubectlBinaryName          = "kubectl"
+	testsDirPath               = "config-connector/tests"
+	originalValuesFileName     = "original_values.yaml"
+	requiredFieldsOnlyFileName = "required_fields_only.yaml"
+	envFileRelativePath        = "testcases/environments.yaml"
+	yamlFileSuffix             = ".yaml"
+)
+
+var (
+	relativePath string
+
+	rootCmd = &cobra.Command{
+		Use:   "ccs-test",
+		Short: "CLI to test Config Connector Solutions",
+		Long:  `CLI to test Config Connector Solutions`,
+	}
+
+	runCmd = &cobra.Command{
+		Use:   "run",
+		Short: "Run a given test by the relative path --path",
+		Long:  "Run a given test by the relative path --path",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Check the required flag.
+			if relativePath == "" {
+				log.Fatal("\"--path\" must be specified to run test")
+				return
+			}
+
+			log.Printf("======Testing solution %q...======\n", relativePath)
+
+			// Calculate the path to testcase directory and solution directory.
+			current, err := os.Getwd()
+			if err != nil {
+				log.Fatalf("error retrieving the current directory: %v", err)
+				return
+			}
+			if !strings.HasSuffix(current, testsDirPath) {
+				log.Fatalf("error running tests under directory: %s. Please "+
+					"follow the instructions in the README.", current)
+				return
+			}
+			testCasePath := filepath.Join(current, "testcases", relativePath)
+			envFilePath := filepath.Join(current, envFileRelativePath)
+
+			parent := filepath.Dir(current)
+			solutionPath := filepath.Join(parent, "solutions", relativePath)
+
+			// Clean up the left over resources if there are any.
+			if err := deleteResources(solutionPath); err != nil {
+				log.Fatalf("error cleaning up resources before running the "+
+					"test. Please clean them up manually: %v", err)
+				return
+			}
+
+			// Fetch the testcase values and run the test.
+			envValues := make(map[string]string)
+			if err := parseYamlToStringMap(envFilePath, envValues); err != nil {
+				log.Fatalf("error retrieving envrionment variables: %v", err)
+				return
+			}
+
+			originalValues := make(map[string]string)
+			if err := parseYamlToStringMap(filepath.Join(testCasePath, originalValuesFileName), originalValues); err != nil {
+				log.Fatalf("error retrieving orginal values: %v", err)
+				return
+			}
+
+			testValues := make(map[string]string)
+			if err := parseYamlToStringMap(filepath.Join(testCasePath, requiredFieldsOnlyFileName), testValues); err != nil {
+				log.Fatalf("error retrieving test values: %v", err)
+				return
+			}
+
+			realValues, err := finalizeValuesFromEnv(envValues, testValues)
+			if err != nil {
+				log.Fatalf("error finalizing test values: %v", err)
+				return
+			}
+
+			if err := runKptTestcase(solutionPath, realValues, originalValues); err != nil {
+				log.Fatalf("test failed for solution %q: %v", relativePath, err)
+				return
+			}
+
+			log.Printf("======Successfully finished the test for solution %q======\n", relativePath)
+		},
+	}
+)
+
+func init() {
+	runCmd.PersistentFlags().StringVarP(&relativePath, "path", "p", "", "[Required] The relative path to the folder of the solution's testcases, e.g. `iam/kpt/member-iam`.")
+	rootCmd.AddCommand(runCmd)
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func parseYamlToStringMap(filePath string, result map[string]string) error {
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("error reading file '%s': %v", filePath, err)
+	}
+	err = yaml.Unmarshal(bytes, &result)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling file '%s': %v", filePath, err)
+	}
+	return nil
+}
+
+func finalizeValuesFromEnv(envValues map[string]string, testValues map[string]string) (map[string]string, error) {
+	realValues := make(map[string]string)
+	for key, value := range testValues {
+		if !strings.HasPrefix(value, "$") {
+			return nil, fmt.Errorf("test value for setter %q is %q, expect a reference, e.g. $ENV_VAR", key, value)
+		}
+		realValue, ok := envValues[strings.TrimPrefix(value, "$")]
+		if !ok {
+			return nil, fmt.Errorf("couldn't find the env var %q", strings.TrimPrefix(value, "$"))
+		}
+		realValues[key] = realValue
+	}
+
+	return realValues, nil
+}
+
+func runKptTestcase(solutionPath string, testValues map[string]string, originalValues map[string]string) error {
+	// Set the kpt setters defined in the testcase.
+	log.Println("======Setting the kpt setters...======")
+	for key, value := range testValues {
+		output, err := exec.Command("kpt", "cfg", "set", solutionPath, key,
+			value, "--set-by", "test").CombinedOutput()
+		if err != nil {
+			errToReturn := fmt.Errorf("error setting setter '%s' with value "+
+				"'%s': %v\nstdout: %s", key, value, err, string(output))
+
+			// Clean up before exit with errors.
+			if err := resetKptSetters(solutionPath, originalValues); err != nil {
+				return concatErrors(
+					"error resetting kpt setters before exiting",
+					err, errToReturn)
+			}
+
+			return errToReturn
+		}
+		log.Printf("%s\n", string(output))
+	}
+	log.Println("======Successfully set the kpt setters======")
+
+	// Apply all the resources.
+	log.Println("======Creating the resources...======")
+	output, err := exec.Command("kubectl", "create", "-f", solutionPath).CombinedOutput()
+	if err != nil {
+		errToReturn := fmt.Errorf("error creating resources: %v\nstdout: %s", err, string(output))
+
+		// Clean up before exit with errors.
+		if err := cleanUp(solutionPath, originalValues); err != nil {
+			return concatErrors(
+				"error cleanning up resources before exiting",
+				err, errToReturn)
+		}
+		return errToReturn
+	}
+	log.Printf("%s\n", string(output))
+	log.Println("======Successfully created the resources======")
+
+	// Wait for all the resources to be ready.
+	if err := verifyReadyCondition(solutionPath); err != nil {
+		errToReturn := fmt.Errorf("error verifying the ready condition: %v", err)
+
+		// Clean up before exit with errors.
+		if err := cleanUp(solutionPath, originalValues); err != nil {
+			return concatErrors(
+				"error cleanning up resources before exiting",
+				err, errToReturn)
+		}
+		return errToReturn
+	}
+
+	// Clean up.
+	return cleanUp(solutionPath, originalValues)
+}
+
+func cleanUp(solutionPath string, originalValues map[string]string) error {
+	resourceErr := deleteResources(solutionPath)
+	setterErr := resetKptSetters(solutionPath, originalValues)
+	if resourceErr != nil || setterErr != nil {
+		return concatErrors(
+			fmt.Sprintf("error cleanning up the test for solution %q. "+
+				"Please manually delete the Config Connector resources and "+
+				"reset the kpt setters", solutionPath),
+			resourceErr, setterErr)
+	}
+	return nil
+}
+
+func verifyReadyCondition(solutionPath string) error {
+	log.Println("======Verifying that all the Config Connector resources are ready...======")
+
+	files, err := ioutil.ReadDir(solutionPath)
+	if err != nil {
+		return fmt.Errorf("error reading solution directory %q: %v", solutionPath, err)
+	}
+
+	for _, file := range files {
+		// We should only verify the YAML config files for Config Connector
+		// resources.
+		fileName := file.Name()
+		if !strings.HasSuffix(fileName, yamlFileSuffix) || strings.Contains(fileName, "namespace") {
+			continue
+		}
+
+		output, err := exec.Command("kubectl", "wait", "--for=condition=ready",
+			"-f", filepath.Join(solutionPath, fileName), "--timeout=60s").CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("resource in file %q is not ready in 60 seconds: %v\nstdout: %s", fileName, err, string(output))
+		}
+		log.Printf("%s\n", string(output))
+
+	}
+
+	log.Println("======All the Config Connector resrouces are ready======")
+	return nil
+}
+
+func deleteResources(solutionPath string) error {
+	log.Println("======Deleting the resources...======")
+	output, err := exec.Command("kubectl", "delete", "-f", solutionPath, "--wait").CombinedOutput()
+	if err != nil {
+		err = fmt.Errorf("error deleting resources: %v\nstdout: %s", err, string(output))
+		if isNotFoundErrorOnly(err) {
+			log.Println(err)
+			log.Println("======Finished deleting the resources======")
+			return nil
+		}
+		return fmt.Errorf("error deleting resources: %v\nstdout: %s", err, string(output))
+	}
+	log.Printf("%s\n", output)
+	log.Println("======Successfully deleted the resources======")
+	return nil
+}
+
+func isNotFoundErrorOnly(err error) bool {
+	numErrors := strings.Count(err.Error(), "Error from server")
+	numNotFoundErrors := strings.Count(err.Error(), "Error from server (NotFound)")
+	return numErrors == numNotFoundErrors
+}
+
+func resetKptSetters(solutionPath string, originalValues map[string]string) error {
+	log.Println("======Resetting the kpt setters...======")
+	for key, value := range originalValues {
+		output, err := exec.Command("kpt", "cfg", "set", solutionPath, key, value, "--set-by", "PLACEHOLDER").CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("error setting setter '%s' back to the original value '%s': %v\nstdout: %s", key, value, err, string(output))
+		}
+		log.Printf("%s\n", string(output))
+	}
+	log.Println("======Successfully reset the kpt setters======")
+	return nil
+}
+
+func concatErrors(msg string, errs ...error) error {
+	errToReturn := errors.New(msg)
+	for _, err := range errs {
+		if err == nil {
+			continue
+		}
+		errToReturn = fmt.Errorf("%v:%v", errToReturn, err)
+	}
+	return errToReturn
+}
