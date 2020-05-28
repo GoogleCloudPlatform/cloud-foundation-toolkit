GoogleCloudPlatform/cloud-foundation-toolkit

commit f9b9173543ddea65b788446c5c6ebd3e104ea177

chore: Add GitHub action for releasing CLI. (#690)

cli/Makefile
@@ -1,6 +1,6 @@
 SHELL := /bin/bash
 
-# need to change this to VERSION=`git describe --tags`
+# Changing this value will trigger a new release
 VERSION=v0.3.2
 BINARY=bin/cft
 GITHUB_REPO=github.com/GoogleCloudPlatform/cloud-foundation-toolkit

cli/cmd/version.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -17,6 +19,6 @@ var versionCmd = &cobra.Command{
 example: CFT CLI version v1.0.0`,
 	Args: cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Println("CFT CLI version", Version)
+		fmt.Println(Version)
 	},
 }

.github/workflows/build-push-cft-devtools.yml
@@ -19,7 +19,7 @@ jobs:
       - uses: GoogleCloudPlatform/github-actions/setup-gcloud@master
         with:
           version: "286.0.0"
-          service_account_key: ${{ secrets.GCR_SA_KEY }}
+          service_account_key: ${{ secrets.GCP_SA_KEY }}
           project_id: ${{ env.PROJECT_ID }}
 
       - name: Build

.github/workflows/release-cli.yml
@@ -1,0 +1,98 @@
+name: Release new CLI versions
+on:
+  push:
+    branches: 
+      - "master"
+    paths:
+      - "cli/Makefile"
+env:
+  CLI_BUCKET: gs://cft-cli
+  RELEASE_URL: https://api.github.com/repos/GoogleCloudPlatform/cloud-foundation-toolkit/releases/latest
+
+jobs:
+  release-new-cli:
+    name: Build and push new CFT dev tools image
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v2
+
+      - uses: GoogleCloudPlatform/github-actions/setup-gcloud@master
+        with:
+          version: "286.0.0"
+          service_account_key: ${{ secrets.GCP_SA_KEY }}
+
+      - name: Retrieve last released version
+        run: |-
+          LAST_VERSION=$(curl --silent "${{env.RELEASE_URL}}" | \
+            jq --raw-output .tag_name)
+          echo ::set-env name=LAST_VERSION::$LAST_VERSION
+
+      - name: Build
+        working-directory: ./cli
+        run: |-
+          make release
+          chmod +x bin/*
+
+      - name: Archive build artifacts
+        uses: actions/upload-artifact@v1
+        with:
+          name: bin
+          path: cli/bin
+
+      - name: Retrieve current version
+        working-directory: ./cli
+        run: |-
+          CURRENT_VERSION=$(./bin/cft-linux-amd64 version)
+          echo ::set-env name=CURRENT_VERSION::$CURRENT_VERSION
+
+      - name: Release new version
+        if: env.LAST_VERSION != env.CURRENT_VERSION
+        working-directory: ./cli
+        run: |-
+          make publish
+
+      - name: Create Release
+        if: env.LAST_VERSION != env.CURRENT_VERSION
+        id: create_release
+        uses: actions/create-release@v1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        with:
+          tag_name: ${{ env.CURRENT_VERSION }}
+          release_name: CLI Release ${{ env.CURRENT_VERSION }}
+          draft: false
+          prerelease: false
+
+      - name: Upload Linux Release
+        if: env.LAST_VERSION != env.CURRENT_VERSION
+        uses: actions/upload-release-asset@v1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        with:
+          upload_url: ${{ steps.create_release.outputs.upload_url }}
+          asset_path: ./cli/bin/cft-linux-amd64
+          asset_name: cft-linux-amd64
+          asset_content_type: application/octet-stream
+
+      - name: Upload Darwin Release
+        if: env.LAST_VERSION != env.CURRENT_VERSION
+        uses: actions/upload-release-asset@v1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        with:
+          upload_url: ${{ steps.create_release.outputs.upload_url }}
+          asset_path: ./cli/bin/cft-darwin-amd64
+          asset_name: cft-darwin-amd64
+          asset_content_type: application/octet-stream
+
+      - name: Upload Windows Release
+        if: env.LAST_VERSION != env.CURRENT_VERSION
+        uses: actions/upload-release-asset@v1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        with:
+          upload_url: ${{ steps.create_release.outputs.upload_url }}
+          asset_path: ./cli/bin/cft-windows-amd64
+          asset_name: cft-windows-amd64
+          asset_content_type: application/octet-stream

commit bb445c3a69f66ee0e3eca8854f238a7b52afd51a

fix(cli): output test cases broken in #690 (#701)
* Fix output test cases broken in #690

* Incrment version

cli/Makefile
@@ -1,7 +1,7 @@
 SHELL := /bin/bash
 
 # Changing this value will trigger a new release
-VERSION=v0.3.2
+VERSION=v0.3.3
 BINARY=bin/cft
 GITHUB_REPO=github.com/GoogleCloudPlatform/cloud-foundation-toolkit
 PLATFORMS := linux windows darwin

cli/cmd/root.go
@@ -3,8 +3,8 @@ package cmd
 import (
 	"os"
 
-	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/cli/scorecard"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/cli/report"
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/cli/scorecard"
 	log "github.com/inconshreveable/log15"
 	"github.com/spf13/cobra"
 )
@@ -26,6 +26,8 @@ var rootCmd = &cobra.Command{
 			// discard logs
 			scorecard.Log.SetHandler(log.DiscardHandler())
 		}
+		// We want to dump to stdout by default
+		cmd.SetOut(cmd.OutOrStdout())
 	},
 }
 

cli/cmd/version.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -19,6 +17,6 @@ var versionCmd = &cobra.Command{
 example: CFT CLI version v1.0.0`,
 	Args: cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(Version)
+		cmd.Println(Version)
 	},
 }

cli/cmd/root_test.go
@@ -41,8 +41,8 @@ func setOutput(rootCommand *cobra.Command, buf *bytes.Buffer) {
 func TestRootCommand(t *testing.T) {
 	rootCmd.SetArgs([]string{})
 	output, err := ExecuteCommand(rootCmd)
-	if output == "" {
-		t.Errorf("expected no output, got: %s", output)
+	if !strings.Contains(output, "Google Cloud Foundation Toolkit CLI") {
+		t.Errorf("expected help output, got: %s", output)
 	}
 	if err != nil {
 		t.Errorf("expected no error, got: %v", err)
@@ -66,6 +66,6 @@ func TestRootCommandWithUnknownCommand(t *testing.T) {
 		t.Errorf("expected to have prefix: %s, actual: %s", prefix, output)
 	}
 	if err == nil {
-		t.Errorf("expected to have erorr but it nil")
+		t.Errorf("expected to have error but it nil")
 	}
 }