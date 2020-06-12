770081c9fe8a277f1f0f7f16b93374e4c9b22e40
Bharath KKB <bharathkrishnakb@gmail.com>
2020-06-12 18:26:37 -0500

    Bump kubectl version (#720)
    * update kubectl
    
    * bump img version

--- infra/build/developer-tools/build/install_kubectl.sh
+++ infra/build/developer-tools/build/install_kubectl.sh
@@ -18,5 +18,5 @@ set -u
 
 cd /build
 
-wget https://storage.googleapis.com/kubernetes-release/release/v1.12.2/bin/linux/amd64/kubectl
+wget https://storage.googleapis.com/kubernetes-release/release/v1.15.12/bin/linux/amd64/kubectl
 install -o 0 -g 0 -m 0755 kubectl /usr/local/bin/kubectl
