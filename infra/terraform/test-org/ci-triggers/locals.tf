094dd1f8d9940c9a525326d4247831200a9aa5d3
bharathkkb <bharathkrishnakb@gmail.com>
2020-10-05 21:23:43 -0500

    feat: add int tests for training

--- infra/terraform/test-org/ci-triggers/locals.tf
+++ infra/terraform/test-org/ci-triggers/locals.tf
@@ -24,6 +24,7 @@ locals {
   ]
   exclude_repos = [
     "ci-example-foundation",
+    "ci-cloud-foundation-training"
   ]
   repo_folder              = { for key, value in data.terraform_remote_state.org.outputs.folders : contains(local.exclude_repos, key) ? replace(key, "ci-", "terraform-") : replace(key, "ci-", "terraform-google-") => replace(value, "folders/", "") if ! contains(local.exclude_folders, key) }
   org_id                   = data.terraform_remote_state.org.outputs.org_id
