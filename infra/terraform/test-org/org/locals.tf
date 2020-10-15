ebb53874872d8998a77b36492fa8dcde8928dfb1
Bharath KKB <bharathkrishnakb@gmail.com>
2020-10-15 01:00:20 -0500

    reorder modules

--- infra/terraform/test-org/org/locals.tf
+++ infra/terraform/test-org/org/locals.tf
@@ -77,10 +77,10 @@ locals {
     "github-actions-runners",
     "healthcare",
     "gke-gitlab",
-    "cloud-router",
     "example-foundation", # Not module
     "anthos-platform",  # Not module
     "cloud-operations",
     "cloud-foundation-training", # Not module
+    "cloud-router",
   ]
 }
