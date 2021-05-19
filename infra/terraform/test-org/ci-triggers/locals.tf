51d3fc18e868cb22395c1dae33fba0081ee44724
bharathkkb <bharathkrishnakb@gmail.com>
2021-05-19 12:04:47 -0500

    add custom repo name mapping

--- infra/terraform/test-org/ci-triggers/locals.tf
+++ infra/terraform/test-org/ci-triggers/locals.tf
@@ -26,13 +26,14 @@ locals {
   # custom mapping of the form name => repo_name used for overriding `terraform-google` prefix
   custom_repo_mapping = {
     "cloud-foundation-training" = "cloud-foundation-training",
-    "example-foundation-app"    = "terraform-example-foundation-app"
+    "example-foundation-app"    = "terraform-example-foundation-app",
+    "anthos-samples"            = "anthos-samples"
   }
   # example foundation has custom test modes
   example_foundation                = { "terraform-example-foundation" = data.terraform_remote_state.org.outputs.ci_repos_folders["example-foundation"] }
   example_foundation_int_test_modes = ["default", "HubAndSpoke"]
 
-  repo_folder              = { for key, value in data.terraform_remote_state.org.outputs.ci_repos_folders : contains(keys(local.custom_repo_mapping), key) ? local.custom_repo_mapping[key] : "terraform-google-${key}" => value if ! contains(local.exclude_folders, value.folder_name) }
+  repo_folder              = { for key, value in data.terraform_remote_state.org.outputs.ci_repos_folders : contains(keys(local.custom_repo_mapping), key) ? local.custom_repo_mapping[key] : "terraform-google-${key}" => value if !contains(local.exclude_folders, value.folder_name) }
   org_id                   = data.terraform_remote_state.org.outputs.org_id
   billing_account          = data.terraform_remote_state.org.outputs.billing_account
   tf_validator_project_id  = data.terraform_remote_state.tf-validator.outputs.project_id
