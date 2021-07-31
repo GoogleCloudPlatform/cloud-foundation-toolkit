1ae41d864ca008e45dfc17b792d54934de334474
bharathkkb <bharathkrishnakb@gmail.com>
2021-07-30 19:22:19 -0500

    remove unnecessary outputs

--- infra/blueprint-test/examples/simple_tf_module_with_test/outputs.tf
+++ infra/blueprint-test/examples/simple_tf_module_with_test/outputs.tf
@@ -33,33 +33,3 @@ output "subnets_names" {
   value       = module.test-vpc-module.subnets_names
   description = "The names of the subnets being created"
 }
-
-output "subnets_ips" {
-  value       = module.test-vpc-module.subnets_ips
-  description = "The IP and cidrs of the subnets being created"
-}
-
-output "subnets_regions" {
-  value       = module.test-vpc-module.subnets_regions
-  description = "The region where subnets will be created"
-}
-
-output "subnets_private_access" {
-  value       = module.test-vpc-module.subnets_private_access
-  description = "Whether the subnets will have access to Google API's without a public IP"
-}
-
-output "subnets_flow_logs" {
-  value       = module.test-vpc-module.subnets_flow_logs
-  description = "Whether the subnets will have VPC flow logs enabled"
-}
-
-output "subnets_secondary_ranges" {
-  value       = module.test-vpc-module.subnets_secondary_ranges
-  description = "The secondary ranges associated with these subnets"
-}
-
-output "route_names" {
-  value       = module.test-vpc-module.route_names
-  description = "The routes associated with this VPC"
-}
