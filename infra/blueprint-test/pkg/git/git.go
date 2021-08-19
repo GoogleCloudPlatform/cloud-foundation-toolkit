ea59d0660df92b95594fb60b1332fd655d79c8f0
bharathkkb <bharathkrishnakb@gmail.com>
2021-08-19 15:22:00 -0500

    git helper

---
+++ infra/blueprint-test/pkg/git/git.go
@@ -0,0 +1,110 @@
+package git
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/utils"
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/shell"
+	"github.com/mitchellh/go-testing-interface"
+)
+
+type CmdCfg struct {
+	gitBinary   string         // git binary
+	dir         string         // dir to execute commands in
+	commitFlags []string       // additional flags when running git commit
+	logger      *logger.Logger // custom logger
+	t           testing.TB     // TestingT or TestingB
+}
+
+type cmdOption func(*CmdCfg)
+
+func WithDir(dir string) cmdOption {
+	return func(f *CmdCfg) {
+		f.dir = dir
+	}
+}
+
+func WithCommitFlags(commitFlags []string) cmdOption {
+	return func(f *CmdCfg) {
+		f.commitFlags = commitFlags
+	}
+}
+
+func WithLogger(logger *logger.Logger) cmdOption {
+	return func(f *CmdCfg) {
+		f.logger = logger
+	}
+}
+
+// NewCmdConfig sets defaults and validates values for git Options.
+func NewCmdConfig(t testing.TB, opts ...cmdOption) *CmdCfg {
+	gitOpts := &CmdCfg{
+		logger:      utils.GetLoggerFromT(),
+		commitFlags: []string{"--author", "BlueprintsTest <blueprints-ci-test@google.com>"},
+		t:           t,
+	}
+	// apply options
+	for _, opt := range opts {
+		opt(gitOpts)
+	}
+	if gitOpts.gitBinary == "" {
+		err := utils.BinaryInPath("git")
+		if err != nil {
+			t.Fatalf("unable to find git in path: %v", err)
+		}
+		gitOpts.gitBinary = "git"
+	}
+	return gitOpts
+}
+
+// RunCmd executes a git command
+func (g *CmdCfg) RunCmdE(args ...string) (string, error) {
+	kptCmd := shell.Command{
+		Command:    g.gitBinary,
+		Args:       args,
+		Logger:     g.logger,
+		WorkingDir: g.dir,
+	}
+	return shell.RunCommandAndGetStdOutE(g.t, kptCmd)
+}
+
+// GetLatestCommit returns latest commit
+func (g *CmdCfg) GetLatestCommit() string {
+	commit, err := g.RunCmdE("rev-parse", "HEAD")
+	if err != nil {
+		g.t.Fatalf("error getting latest commit: %v", err)
+	}
+	return commit
+}
+
+// Init run git init
+func (g *CmdCfg) Init() {
+	_, err := g.RunCmdE("init")
+	if err != nil {
+		g.t.Fatalf("error running git init: %v", err)
+	}
+}
+
+// AddAll stages all changes.
+func (g *CmdCfg) AddAll() {
+	_, err := g.RunCmdE("add", "-A")
+	if err != nil {
+		g.t.Fatalf("error running git add: %v", err)
+	}
+}
+
+// CommitWithMsg commits changes with commit msg.
+func (g *CmdCfg) CommitWithMsg(msg string) {
+	_, err := g.RunCmdE(append([]string{"commit", "-m", fmt.Sprintf("%q", msg)}, g.commitFlags...)...)
+	if err != nil {
+		g.t.Fatalf("error running git commit: %v", err)
+	}
+}
+
+// CommitWithMsg commits changes with a generated commit msg.
+func (g *CmdCfg) Commit() {
+	currentTime := time.Now()
+	g.CommitWithMsg(fmt.Sprintf("commit %s", currentTime.Format(time.RFC1123)))
+}
