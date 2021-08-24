87bcbce637ef045b7716afec1b8c5265554b840d
bharathkkb <bharathkrishnakb@gmail.com>
2021-08-23 20:31:32 -0500

    test typo

--- infra/blueprint-test/examples/krm_blueprints_with_test/test/integration/vpc-example-custom/vpc_custom_test.go
+++ infra/blueprint-test/examples/krm_blueprints_with_test/test/integration/vpc-example-custom/vpc_custom_test.go
@@ -17,7 +17,7 @@ func TestVPCCustomBlueprint(t *testing.T) {
 		func(assert *assert.Assertions) {
 			networkBlueprint.DefaultVerify(assert)
 			op := gcloud.Run(t, fmt.Sprintf("compute networks describe custom-network --project %s", networkBlueprint.ValFromEnv("PROJECT_ID")))
-			assert.Equal("GLOBAL", op.Get("routingConfig.routingMode").String(), "should have be GLOBAL")
+			assert.Equal("GLOBAL", op.Get("routingConfig.routingMode").String(), "should be GLOBAL")
 			assert.Equal("false", op.Get("autoCreateSubnetworks").String(), "autoCreateSubnetworks should not be enabled")
 		})
 	networkBlueprint.Test()
