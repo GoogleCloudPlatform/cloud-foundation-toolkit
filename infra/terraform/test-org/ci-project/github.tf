26017dc3e56144cc72fac1cb16f4531f1b9e6a1d
bharathkkb <bharathkrishnakb@gmail.com>
2021-10-19 13:31:12 -0500

    feat: setup WIF for devtools image push

--- infra/terraform/test-org/ci-project/github.tf
+++ infra/terraform/test-org/ci-project/github.tf
@@ -20,8 +20,14 @@ resource "github_actions_secret" "infra_secret_gcr_project" {
   plaintext_value = local.project_id
 }
 
-resource "github_actions_secret" "infra_secret_gcr_key" {
+resource "github_actions_secret" "wif_provider" {
   repository      = local.gh_repos.infra
-  secret_name     = "GCP_SA_KEY"
-  plaintext_value = module.service_accounts.key
+  secret_name     = "GCP_WIF_PROVIDER"
+  plaintext_value = module.oidc.provider_name
+}
+
+resource "github_actions_secret" "wif_sa" {
+  repository      = local.gh_repos.infra
+  secret_name     = "GCP_WIF_SA_EMAIL"
+  plaintext_value = module.service_accounts.service_account.email
 }
