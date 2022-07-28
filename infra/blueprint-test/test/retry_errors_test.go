9aec8c4f36ecd70843cf526f474babc96b262ea0
Daniel da Silva Andrade <dandrade@ciandt.com>
2022-07-28 12:04:04 -0300

    add tests for retryable errors

---
+++ infra/blueprint-test/test/retry_errors_test.go
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRetryErrors(t *testing.T) {
+	bpt := tft.NewTFBlueprintTest(t,
+		tft.WithTFDir("../examples/retry_errors"),
+		tft.WithSetupPath("./setup/simple_tf_module"),
+		tft.WithRetryableTerraformErrors(tft.CommonRetryableErrors, 2, 3*time.Second),
+	)
+	bpt.DefineVerify(func(assert *assert.Assertions) {})
+	bpt.DefineTeardown(func(assert *assert.Assertions) {})
+	bpt.DefineApply(
+		func(assert *assert.Assertions) {
+			out, err := terraform.ApplyE(t, bpt.GetTFOptions())
+			assert.Contains(out, "SERVICE_DISABLED")
+			errMsg := err.Error()
+			assert.Equal(errMsg, "'terraform [apply -input=false -auto-approve -lock=false]' unsuccessful after 2 retries")
+		})
+	bpt.Test()
+}
