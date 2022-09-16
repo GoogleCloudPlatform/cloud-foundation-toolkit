bfd3387e8cc21c08cc48ae57d4120371aed68faa
Andrew Peabody <andrewpeabody@google.com>
2022-09-16 15:02:30 -0700

    Add terraform-google-media-cdn-vod to test-org

--- infra/terraform/test-org/org/locals.tf
+++ infra/terraform/test-org/org/locals.tf
@@ -86,6 +86,7 @@ locals {
     "module-template",
     "vault",
     "docs-samples",
+    "media-cdn-vod",
   ]
   # These should also be added to test-org/github/branch.tf#L19 till the todo is resolved.
   gcp_org_modules = [
