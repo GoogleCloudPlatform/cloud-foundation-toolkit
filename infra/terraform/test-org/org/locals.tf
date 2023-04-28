0bbaaee7f67f44bf44a7ee465d546e7a3d15c64b
Nim Jayawardena <i.am.nim.jay@gmail.com>
2023-04-28 09:59:25 -0400

    fix: List individual members of dee-platform-ops
    List of members:
    https://github.com/orgs/GoogleCloudPlatform/teams/dee-platform-ops/members

--- infra/terraform/test-org/org/locals.tf
+++ infra/terraform/test-org/org/locals.tf
@@ -113,7 +113,7 @@ locals {
       short_name  = "ecommerce-microservices"
       org         = "GoogleCloudPlatform"
       description = "Deploys a web-based ecommerce app into a multi-cluster Google Kubernetes Engine setup."
-      owners      = ["@GoogleCloudPlatform/dee-platform-ops"]
+      owners      = ["arbrown", "bourgeoisor", "donmccasland", "minherz", "NimJay", "Shabirmean"]
     },
     {
       name         = "terraform-example-foundation"
