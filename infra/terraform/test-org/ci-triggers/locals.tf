0f64f8e60d5c3c682aa7887a1c89ea8e6a8c6d3c
Andrew Peabody <andrewpeabody@google.com>
2023-05-05 22:05:13 +0000

    minor fixes

--- infra/terraform/test-org/ci-triggers/locals.tf
+++ infra/terraform/test-org/ci-triggers/locals.tf
@@ -36,8 +36,8 @@ locals {
     "ecommerce-microservices"     = "terraform-ecommerce-microservices-on-gke"
     "deploy-java-gke"             = "terraform-example-deploy-java-gke"
     "Migrate-Legacy-Java-App-GKE" = "Migrate-Legacy-Java-App-GKE"
-    "large-data-sharing-java-app" = "terraform-large-data-sharing-java-webapp"
-    "large-data-sharing-go-app"   = "terraform-large-data-sharing-golang-webapp"
+    "large-data-sharing-java-app" = "app-large-data-sharing-java"
+    "large-data-sharing-go-app"   = "app-large-data-sharing-golang"
     "java-dynamic-point-of-sale"  = "terraform-example-java-dynamic-point-of-sale"
   }
   # example foundation has custom test modes
