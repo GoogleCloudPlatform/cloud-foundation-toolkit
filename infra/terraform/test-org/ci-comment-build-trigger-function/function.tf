930ac1442ed1bd14578fe7b26b5ed6e74e79e9aa
Andrew Peabody <andrewpeabody@google.com>
2023-05-08 18:17:24 +0000

    add TODO

--- infra/terraform/test-org/ci-comment-build-trigger-function/function.tf
+++ infra/terraform/test-org/ci-comment-build-trigger-function/function.tf
@@ -14,45 +14,45 @@
  * limitations under the License.
  */
 
-/*
-locals {
-  project_id = "cloud-foundation-cicd"
-}
+# TODO: no longer in-use, remove per https://github.com/GoogleCloudPlatform/cloud-foundation-toolkit/issues/1537
 
-module "gcf_service_account" {
-  source     = "terraform-google-modules/service-accounts/google"
-  version    = "~> 4.1"
-  project_id = local.project_id
-  names      = ["pr-comment-cf-sa"]
-  project_roles = [
-    "${local.project_id}=>roles/cloudbuild.builds.editor"
-  ]
-}
+# locals {
+#   project_id = "cloud-foundation-cicd"
+# }
 
-resource "random_id" "suffix" {
-  byte_length = 4
-}
+# module "gcf_service_account" {
+#   source     = "terraform-google-modules/service-accounts/google"
+#   version    = "~> 4.1"
+#   project_id = local.project_id
+#   names      = ["pr-comment-cf-sa"]
+#   project_roles = [
+#     "${local.project_id}=>roles/cloudbuild.builds.editor"
+#   ]
+# }
 
-module "pr_comment_build_function" {
-  source                = "terraform-google-modules/event-function/google"
-  version               = "~> 2.3"
-  name                  = "pr-comment-downstream-builder-${random_id.suffix.hex}"
-  project_id            = local.project_id
-  region                = "us-central1"
-  description           = "Launches a downstream build that comments on a PR."
-  entry_point           = "main"
-  runtime               = "python37"
-  source_directory      = "${path.module}/function_source"
-  service_account_email = module.gcf_service_account.email
-  bucket_force_destroy  = true
+# resource "random_id" "suffix" {
+#   byte_length = 4
+# }
 
-  environment_variables = {
-    CLOUDBUILD_PROJECT = local.project_id
-  }
+# module "pr_comment_build_function" {
+#   source                = "terraform-google-modules/event-function/google"
+#   version               = "~> 2.3"
+#   name                  = "pr-comment-downstream-builder-${random_id.suffix.hex}"
+#   project_id            = local.project_id
+#   region                = "us-central1"
+#   description           = "Launches a downstream build that comments on a PR."
+#   entry_point           = "main"
+#   runtime               = "python37"
+#   source_directory      = "${path.module}/function_source"
+#   service_account_email = module.gcf_service_account.email
+#   bucket_force_destroy  = true
 
-  event_trigger = {
-    event_type = "google.pubsub.topic.publish"
-    resource   = "projects/${local.project_id}/topics/cloud-builds"
-  }
-}
-*/
+#   environment_variables = {
+#     CLOUDBUILD_PROJECT = local.project_id
+#   }
+
+#   event_trigger = {
+#     event_type = "google.pubsub.topic.publish"
+#     resource   = "projects/${local.project_id}/topics/cloud-builds"
+#   }
+# }
