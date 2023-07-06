55d65307e8626a2809bcdce428058d4071d23253
Awais Malik <awmalik@google.com>
2023-07-06 11:55:41 -0700

    allowlist for invalid members

--- infra/terraform/test-org/org/github.tf
+++ infra/terraform/test-org/org/github.tf
@@ -29,6 +29,12 @@ locals {
   invalid_owners = setsubtract(local.owners, local.org_members)
 }
 
+variable "temp_allow_invalid_owners" {
+  type = list(string)
+  description = "Googlers that are added as owners on TF blueprint repos but are not part of the GCP or TGM orgs."
+  default = []  
+}
+
 data "github_organization" "tgm" {
   name = "terraform-google-modules"
 }
