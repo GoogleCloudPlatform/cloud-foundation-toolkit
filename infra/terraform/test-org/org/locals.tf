9b256fb652871301cb950ffde48d7658f7bf0b39
Grant Sorbo <gtsorbo@google.com>
2023-07-24 09:59:17 -0500

    fix: formatting

--- infra/terraform/test-org/org/locals.tf
+++ infra/terraform/test-org/org/locals.tf
@@ -732,12 +732,12 @@ locals {
       enable_periodic = true
     },
     {
-      name            = "terraform-google-secured-data-warehouse-onprem-ingest"
-      short_name      = "sdw-onprem-ingest"
-      org             = "GoogleCloudPlatform"
-      description     = "Secured Data Warehouse blueprint variant for ingesting encrypted data from on-prem sources"
-      owners          = ["lanreogunmola"]
-      topics          = join(",", [local.common_topics.da, local.common_topics.security, local.common_topics.e2e])
+      name        = "terraform-google-secured-data-warehouse-onprem-ingest"
+      short_name  = "sdw-onprem-ingest"
+      org         = "GoogleCloudPlatform"
+      description = "Secured Data Warehouse blueprint variant for ingesting encrypted data from on-prem sources"
+      owners      = ["lanreogunmola"]
+      topics      = join(",", [local.common_topics.da, local.common_topics.security, local.common_topics.e2e])
     },
   ]
 }
