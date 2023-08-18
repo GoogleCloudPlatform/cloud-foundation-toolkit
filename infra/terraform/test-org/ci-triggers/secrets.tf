8143a53b9ea542dd6bbe4d75a7b30833a5ab8856
Awais Malik <awmalik@google.com>
2023-08-18 11:56:29 -0700

    feat: adds secrets for tf-cloud-agents modules

---
+++ infra/terraform/test-org/ci-triggers/secrets.tf
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "google_secret_manager_secret" "tfe_token" {
+  project   = local.project_id
+  secret_id = "tke-token"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_iam_member" "tfe_token_member" {
+  project   = google_secret_manager_secret.tfe_token.project
+  secret_id = google_secret_manager_secret.tfe_token.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "group:${data.terraform_remote_state.org.outputs.cft_ci_group}"
+}
