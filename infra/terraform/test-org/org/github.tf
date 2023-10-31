c52a94cf75e0ffb31c5ba1dfd7767851c228eb2d
Imran Nayer <imrannayer@google.com>
2023-10-30 22:19:53 -0500

    chore: add owner for cloudsql and cloudops (#1913)
    Co-authored-by: Awais Malik <awmalik@google.com>

--- infra/terraform/test-org/org/github.tf
+++ infra/terraform/test-org/org/github.tf
@@ -34,6 +34,7 @@ variable "temp_allow_invalid_owners" {
   description = "Googlers added as owners on TF blueprint repos but are not part of the GCP or TGM orgs yet."
   default = [
     "dulacp",
+    "nidhi0710", # remove once heynidhi@ is added to GCP org
   ]
 }
 
--- infra/terraform/test-org/org/locals.tf
+++ infra/terraform/test-org/org/locals.tf
@@ -113,7 +113,7 @@ locals {
       name        = "terraform-google-kubernetes-engine"
       org         = "terraform-google-modules"
       description = "Configures opinionated GKE clusters"
-      owners      = ["Jberlinsky", "ericyz"]
+      owners      = ["ericyz"]
       topics      = join(",", [local.common_topics.compute, local.common_topics.containers])
     },
     {
@@ -277,6 +277,7 @@ locals {
       org         = "terraform-google-modules"
       description = "Manages Google Cloud's operations suite (Cloud Logging and Cloud Monitoring)"
       topics      = local.common_topics.ops
+      owners      = ["imrannayer"]
     },
     {
       name        = "terraform-google-cloud-router"
@@ -476,7 +477,7 @@ locals {
       org         = "terraform-google-modules"
       description = "Creates a Cloud SQL database instance"
       topics      = local.common_topics.db
-      owners      = ["isaurabhuttam"]
+      owners      = ["isaurabhuttam", "imrannayer"]
     },
     {
       name        = "terraform-google-startup-scripts"
