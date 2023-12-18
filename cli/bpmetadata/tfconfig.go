89cebaa1b71b66844493616ae30c2e7a0a97d5b9
Ray Zhang <rzy@google.com>
2023-12-18 08:34:52 +0000

    chore: omit null defaultValue in bpmetadata

--- cli/bpmetadata/tfconfig.go
+++ cli/bpmetadata/tfconfig.go
@@ -231,12 +231,12 @@ func getBlueprintVariable(modVar *tfconfig.Variable) *BlueprintVariable {
 		Required:    modVar.Required,
 		VarType:     modVar.Type,
 	}
-
-	vl, err := structpb.NewValue(modVar.Default)
-	if err == nil {
-		v.DefaultValue = vl
+	if modVar.Default != nil {
+		vl, err := structpb.NewValue(modVar.Default)
+		if err == nil {
+			v.DefaultValue = vl
+		}
 	}
-
 	return v
 }
 
