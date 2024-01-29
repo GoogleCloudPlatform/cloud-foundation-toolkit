4fc66a865dd2296fc38943f8e3a48040f09e256d
Andrew Peabody <andrewpeabody@google.com>
2024-01-29 18:19:37 +0000

    import new repos

--- infra/terraform/test-org/github/versions.tf
+++ infra/terraform/test-org/github/versions.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Google LLC
+ * Copyright 2019-2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 
 terraform {
-  required_version = ">= 1.4.4"
+  required_version = ">= 1.7.0"
   required_providers {
     github = {
       source  = "integrations/github"
