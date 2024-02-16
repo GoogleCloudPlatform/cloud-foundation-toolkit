015348f1d723c8de06cbbaaa8a676a21bce175aa
Andrew Peabody <andrewpeabody@google.com>
2024-02-16 20:00:40 +0000

    chore: add periodic support for docs-samples

--- infra/terraform/test-org/ci-triggers/scheduler.tf
+++ infra/terraform/test-org/ci-triggers/scheduler.tf
@@ -1,5 +1,21 @@
+/**
+ * Copyright 2023-2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 locals {
-  periodic_repos = toset([for m in data.terraform_remote_state.org.outputs.modules : m.name if lookup(m, "enable_periodic", false)])
+  periodic_repos = toset([for item in data.terraform_remote_state.org.outputs.periodic_repos : contains(keys(local.custom_repo_mapping), item) ? local.custom_repo_mapping[item] : item])
 }
 
 resource "google_service_account" "periodic_sa" {
