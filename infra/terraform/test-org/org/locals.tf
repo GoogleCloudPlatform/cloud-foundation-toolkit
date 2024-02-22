add1921f784928ec0115c0baa1c7ae4801ac7081
Andrew Peabody <andrewpeabody@google.com>
2024-02-21 17:15:24 -0800

    chore(cft): add kubernetes-engine owner

--- infra/terraform/test-org/org/locals.tf
+++ infra/terraform/test-org/org/locals.tf
@@ -113,7 +113,7 @@ locals {
       name        = "terraform-google-kubernetes-engine"
       org         = "terraform-google-modules"
       description = "Configures opinionated GKE clusters"
-      owners      = ["ericyz"]
+      owners      = ["ericyz", "gtsorbo"]
       topics      = join(",", [local.common_topics.compute, local.common_topics.containers])
     },
     {
