14b55a139a1e9d87ce9405f2f83c78dc56120ce9
Nim Jayawardena <i.am.nim.jay@gmail.com>
2024-05-01 08:20:04 -0400

    Remove locals related to JSS 11.1/11.2

--- infra/terraform/test-org/ci-triggers/locals.tf
+++ infra/terraform/test-org/ci-triggers/locals.tf
@@ -38,8 +38,6 @@ locals {
     "deploy-java-multizone"       = "terraform-example-deploy-java-multizone"
     "ecommerce-microservices"     = "terraform-ecommerce-microservices-on-gke"
     "deploy-java-gke"             = "terraform-example-deploy-java-gke"
-    "large-data-sharing-java-app" = "terraform-large-data-sharing-java-webapp"
-    "large-data-sharing-go-app"   = "terraform-large-data-sharing-golang-webapp"
     "java-dynamic-point-of-sale"  = "terraform-example-java-dynamic-point-of-sale"
     "ml-image-annotation-gcf"     = "terraform-ml-image-annotation-gcf"
     "genai-doc-summarization"     = "terraform-genai-doc-summarization"
