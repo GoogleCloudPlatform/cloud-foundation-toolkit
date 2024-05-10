ea40fba14d00ec0e9ceab6d3131513cc3058ae97
Andrew Peabody <andrewpeabody@google.com>
2024-05-10 21:18:09 +0000

    chore(CI): enable cai and scc cleanup

--- infra/terraform/test-org/test-cleanup/project.tf
+++ infra/terraform/test-org/test-cleanup/project.tf
@@ -16,7 +16,7 @@
 
 module "cft-manager-project" {
   source  = "terraform-google-modules/project-factory/google"
-  version = "~> 14.0"
+  version = "~> 15.0"
 
   name              = "cft-project-manager"
   random_project_id = true
