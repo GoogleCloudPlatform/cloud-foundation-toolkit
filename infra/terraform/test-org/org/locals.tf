1686cc3e2b03946f728ad4b2d4023954cd5e591d
sjswerdlow <109298351+sjswerdlow@users.noreply.github.com>
2024-05-17 14:51:55 -0400

    Adding megela as an owner of the terraform-google-sap repository.
    Adding megela as an owner of the terraform-google-sap repository.

--- infra/terraform/test-org/org/locals.tf
+++ infra/terraform/test-org/org/locals.tf
@@ -458,7 +458,7 @@ locals {
       org         = "terraform-google-modules"
       description = "Deploys SAP products"
       topics      = local.common_topics.compute
-      owners      = ["sjswerdlow"]
+      owners      = ["sjswerdlow", "megela"]
     },
     {
       name        = "terraform-google-scheduled-function"
