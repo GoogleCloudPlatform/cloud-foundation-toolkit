64ffe660c8fcf1ed0602a590f672ae1b486b005a
Andrew Peabody <andrewpeabody@google.com>
2024-06-05 17:02:58 +0000

    chore: add default ingress rule

--- infra/terraform/test-org/org/policy.tf
+++ infra/terraform/test-org/org/policy.tf
@@ -25,3 +25,36 @@ resource "google_access_context_manager_access_policy" "access_policy" {
   parent   = "organizations/${local.org_id}"
   title    = "default policy"
 }
+
+// For ingress policy
+resource "google_access_context_manager_service_perimeter" "default_perimeter" {
+  parent = "accesspolicies/${google_access_context_manager_access_policy.access_policy.name}"
+  name   = "accesspolicies/${google_access_context_manager_access_policy.access_policy.name}/serviceperimeters/default_perimeter"
+  title  = "Default Perimeter"
+  lifecycle {
+    ignore_changes = [status[0].resources]
+  }
+}
+
+// For AlloyDB PSC
+resource "google_access_context_manager_service_perimeter_ingress_policy" "deafult_ingress_policy" {
+  perimeter = google_access_context_manager_service_perimeter.default_perimeter.name
+  ingress_from {
+    identity_type = "ANY_SERVICE_ACCOUNT"
+    sources {
+      access_level = "*"
+    }
+  }
+  ingress_to {
+    resources = ["*"]
+    operations {
+      service_name = "networkconnectivity.googleapis.com"
+      method_selectors {
+        method = "*"
+      }
+    }
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
