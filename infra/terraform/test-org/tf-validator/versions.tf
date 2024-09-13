7b8b9f2768817ce3014119a0d71d11a7f800f140
Andrew Peabody <andrewpeabody@google.com>
2024-09-13 17:16:43 +0000

    fixes

--- infra/terraform/test-org/tf-validator/versions.tf
+++ infra/terraform/test-org/tf-validator/versions.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Google LLC
+ * Copyright 2019-2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,5 +21,10 @@ terraform {
       source  = "hashicorp/google"
       version = "< 7"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "< 7"
+    }
+
   }
 }
