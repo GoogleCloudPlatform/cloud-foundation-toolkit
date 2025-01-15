55f37dbee90d9974071951b748d37f8fea98b095
Bharath KKB <bharathkrishnakb@gmail.com>
2025-01-15 13:07:18 -0600

    update maintainers

--- infra/terraform/test-org/org/locals.tf
+++ infra/terraform/test-org/org/locals.tf
@@ -918,14 +918,12 @@ locals {
       org         = "GoogleCloudPlatform"
       description = "Creates and manages AppHub resources"
       maintainers = ["q2w"]
-      admins      = ["bharathkkb"]
     },
     {
       name        = "terraform-google-regional-lb-http"
       org         = "GoogleCloudPlatform"
       description = "Creates a regional HTTP load balancer using forwarding rules"
-      maintainers = ["qz267"]
-      admins      = ["q2w", "bharathkkb"]
+      maintainers = ["q2w", "qz267"]
     },
   ]
 }
